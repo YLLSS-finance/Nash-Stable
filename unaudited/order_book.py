8d0e7d1694d10674b738eab5b7e9f57e17ecb3e9
yllssnospinning <mrnospinning@gmail.com>
2026-02-05 21:15:52 +0800

    Fixed a bug regarding the update of links while adding a new limit order to an existing price level in the order book

--- unaudited/order_book.py
+++ unaudited/order_book.py
@@ -21,19 +21,21 @@ class order_book:
         self.globalOrders.orders[order_idx][8] = tail
 
     def add_order(self, idx, order):
-        order_side = order[5]
-        order_price = self.priceMapping[order_side](order[4])
+        order_price, order_side, order_quantity = order[4:7]
+        order_price = self.priceMapping[order_side](order_price)
 
         side_level = self.levels[order_side]
         side_level_prices = self.levelPrices[order_side]
         if order_price not in side_level:
             # head price, tail price, head order, tail order, cum orders, cum qty
-            side_level[order_price] = None
+            side_level[order_price] = [None, None, idx, idx, 1, order[6]]
+            order_price_level = side_level[order_price]
             new_price_index = side_level_prices.index(order_price)
             num_prices = len(side_level_prices)
 
             head_price, tail_price = None, None
 
+            # Handle links of head and tail prices, if they exist
             if new_price_index != 0:
                 head_price = side_level_prices[new_price_index - 1]
                 head_price_level = side_level[head_price]
@@ -48,6 +50,8 @@ class order_book:
                 tail_price_head_order = tail_price_level[2]
                 self.set_order_head(order_idx=tail_price_head_order, head=idx)
 
+            order_price_level[0:2] = [head_price, tail_price]
+
             # Update the top of book price
             cur_tob = self.topOfBook[order_side]
             if cur_tob is None:
@@ -57,6 +61,18 @@ class order_book:
 
         else:
             order_price_level = side_level[order_price]
+
+            # For the head order of the tail price level of the new order,
+            # set its head to that of the new order,
+            # as it directly connects with it (new order is at the end of the queue at its price level,
+            # so the first order at the immediately worse price level has to update its head link to it)
             tail_price_level = side_level[order_price_level[1]]
-            tail_price_tail_order = tail_price_level[3]
-            self.set_order_head(order_idx=tail_price_tail_order, head=idx)
+            tail_price_head_order = tail_price_level[2]
+            self.set_order_head(order_idx=tail_price_head_order, head=idx)
+
+            # set the tail of the order's price level to that of the new order
+            order_price_level[3] = idx
+
+            # update cumulative order count and order quantity
+            order_price_level[4] += 1
+            order_price_level[5] += order_quantity
