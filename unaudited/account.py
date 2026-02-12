c3ac9b8bbae776db0eafb2b229cd8bccf4ad6222
yllssnospinning <yllss.lvis@gmail.com>
2026-02-12 08:31:18 +0800

    added slots to account object it now functions just as a structured data object which allows for more explicit manipulation

--- unaudited/account.py
+++ unaudited/account.py
@@ -2,8 +2,9 @@ from position import position
 
 
 # TODO: Add slots
-@dataclass
 class account:
+    __slots__ = ("balance", "positions", "usedOrders", "availableOrders")
+
     def __init__(self):
         self.balance = [0, 0]
         self.positions = {}
