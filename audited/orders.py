acd365817bcfb4b40f324c348a342ac3f5118135
yllssnospinning <yllss.lvis@gmail.com>
2026-02-20 14:02:00 +0800

    Made new orders.py with multiple np lists to avoid the np dimensional overhead

--- audited/orders.py
+++ audited/orders.py
@@ -4,3 +4,24 @@ import numpy as np
 class orders:
     def __init__(self, max_accounts, orders_per_account):
         self.maxAccounts = int(max_accounts)
+        self.ordersPerAccount = int(orders_per_account)
+        self.accountMapping = {}
+
+        array_length = self.maxAccounts * self.ordersPerAccount
+
+        self.timestamp = np.empty(array_length, dtype=np.int32)
+        self.mpid = np.empty(array_length, dtype=np.int32)
+        self.contractID = np.empty(array_length, dtype=np.int32)
+        self.price = self.contractID = np.empty(array_length, dtype=np.int16)
+        self.side = np.empty(array_length, dtype=np.int8)
+        self.qty = np.empty(array_length, dtype=np.uint16)
+        self.headOrder = self.contractID = np.empty(array_length, dtype=np.int32)
+        self.tailOrder = np.empty(array_length, dtype=np.int32)
+        self.marginManagerIdx = np.empty(array_length, dtype=np.int32)
+
+    def add_account(self, mpid):
+        if mpid in self.accountMapping:
+            return
+        acct_start_idx = len(self.accountMapping.keys()) * self.ordersPerAccount
+        self.accountMapping[mpid] = acct_start_idx
+        return acct_start_idx
